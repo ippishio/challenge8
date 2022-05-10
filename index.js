71e4cf045492c1b5fd46e4305d20a365f0013226
ippishio <waterlava535@gmail.com>
2022-05-10 21:40:58 +0300

    test commit

---
+++ index.js
@@ -0,0 +1,158 @@
+
+const { providers } = nearApi;
+const { utils } = nearApi;
+const near = new nearApi.Near({
+    keyStore: new nearApi.keyStores.BrowserLocalStorageKeyStore(),
+    networkId: 'testnet',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org'
+  });
+
+  // connect to the NEAR Wallet
+const login = document.getElementById("login");
+const wallet = new nearApi.WalletConnection(near, 'my-app');
+const cancel = document.getElementById("cancel");
+var canPlay= false;
+const ftContract = new nearApi.Contract(wallet.account(), 'wrap.testnet', {
+    changeMethods: ['ft_transfer_call', 'near_deposit'],
+});
+
+const roketo = new nearApi.Contract(wallet.account(), 'streaming-r-v2.dcversus.testnet', {
+    viewMethods: ['get_account', 'get_account_outgoing_streams', 'get_stream'],
+    changeMethods: [ 'stop_stream']
+});
+const provider = new providers.JsonRpcProvider(
+    "https://archival-rpc.testnet.near.org"
+  );
+  const TX_HASH = new URLSearchParams(window.location.search).get('transactionHashes');
+  var lastStream;
+if (!wallet.isSignedIn()) {
+    login.textContent = "login bitch";
+} else {
+    const roketoAccount = await roketo.get_account({
+        "account_id": wallet.getAccountId()
+    });
+    const lastAccountStream = await roketo.get_stream({
+        "stream_id" : roketoAccount.last_created_stream
+    });
+    if (lastAccountStream.description == "gamesmartpay") {
+        lastStream = lastAccountStream;
+        console.log("last stream is gamesmartpay, hash: "+lastAccountStream.id);
+        console.log(lastAccountStream); 
+    } else {
+    const response = await roketo.get_account_outgoing_streams({
+        "account_id": wallet.getAccountId().toString(),
+            "from": 0,
+            "limit": 9999,
+        
+    });
+    console.log("total streams: "+response.length);
+    var count = 0;
+    
+    response.forEach(stream => {
+        
+        if(stream.description == "gamesmartpay") {
+            count += 1
+            console.log("finded gamesmartpay stream, hash: "+ stream.id);
+            lastStream = stream;
+        }
+    });
+    console.log("total game pay streams: "+count);
+}
+    if(TX_HASH) {
+        if(!(new URLSearchParams(window.location.search).get('errorCode'))) {
+           console.log("no error");
+          const result = await provider.txStatus(TX_HASH, wallet.getAccountId().toString());
+          console.log(window.atob(result['status']['SuccessValue']));
+         // const randID = eval(window.atob(result['status']['SuccessValue']))[0];
+          if(window.atob(result['status']['SuccessValue']) == '') {
+           
+          } else if(window.atob(result['status']['SuccessValue']) == '"0"') {
+              document.getElementById("game").style.visibility = "visible";
+              login.style.visibility = "hidden";
+
+          }
+        } else {
+          console.log("error");
+         }
+     
+       console.log(TX_HASH);
+       }
+}
+document.getElementById("deposit").addEventListener('click', () => {
+    if(wallet.isSignedIn()) {
+        ftContract.near_deposit({}, 200000000000000, '10000000000000000000000000');
+    } else {
+    wallet.requestSignIn({
+        contractId: 'wrap.testnet',
+        methodNames: ['ft_transfer_call', 'near_deposit']
+      });
+}});
+var alreadyStreamedAmount;
+function UpdateTotal() {
+    if(lastStream.status != "Paused") {
+        alreadyStreamedAmount = Math.round(((Date.now()/1000-lastStream.timestamp_created/1000000000)*utils.format.formatNearAmount(lastStream.tokens_per_sec))*1000)/1000;
+        canPlay = true;
+    } else {
+        alreadyStreamedAmount = utils.format.formatNearAmount(lastStream.tokens_total_withdrawn);
+        canPlay = false;
+    }
+    document.getElementById("total").textContent = alreadyStreamedAmount+"/1 N"
+}
+if(lastStream){
+    if(alreadyStreamedAmount < 1) {
+    console.log("laststream")
+  if(lastStream.status != "Paused") {
+  var t = setInterval(UpdateTotal,1000);
+  cancel.style.visibility = "visible";
+    console.log("active stream");
+} } else {
+    document.getElementById("total").textContent = "Game buyed!";
+    clearInterval(t);
+    canPlay = true;
+
+} 
+}
+if(canPlay) {
+    login.textContent = "Play Game";
+}
+cancel.addEventListener('click', () => {
+    roketo.stop_stream({"stream_id":lastStream.id},200000000000000, 1);
+})
+login.addEventListener('click', () => {
+    if(wallet.isSignedIn()) {
+        if(!canPlay) {
+        ftContract.ft_transfer_call({
+            receiver_id: 'streaming-r-v2.dcversus.testnet',
+            amount: '1000000000000000000000000', // 1 NEAR
+            memo: 'Roketo transfer',
+            msg: JSON.stringify({
+                Create: {
+                    request: {
+                        "owner_id": "ippishio.testnet",
+                        "receiver_id": "bebrab.testnet",
+                        "tokens_per_sec": 100000000000000000000, // 1 month for 1 NEAR
+                        "description": "gamesmartpay",
+                        "is_auto_start_enabled": true,
+                    }
+                }
+            }),
+        }, 200000000000000, 1);
+    } else {
+        location.replace("./game.html")
+    }
+    
+    } else {
+    wallet.requestSignIn({
+        contractId: 'wrap.testnet',
+        methodNames: ['ft_transfer_call', 'near_deposit']
+      });
+    }
+})
+document.getElementById("logout").addEventListener('click', () => {
+    wallet.signOut();
+    location.href = 'https://'+window.location.hostname;
+    window.location.reload();
+    console.log("logged out");
+   });
+// only for NEAR, we need deposit NEAR on wrap
